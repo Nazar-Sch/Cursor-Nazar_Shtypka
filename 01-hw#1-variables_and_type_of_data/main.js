96ef00d204d20cc12b4b87102c5f4bcbd2069e52
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-01-29 17:05:45 +0200

    home work #1 - done

--- 01-hw#1-variables_and_type_of_data/main.js
+++ 01-hw#1-variables_and_type_of_data/main.js
@@ -1,36 +1,88 @@
-let laptopHp = 15.678;
-let laptopApple = 123.965;
-let laptopLenovo = 90.2345;
-console.log(`Laptop HP: ` + laptopHp);
-console.log(`Laptop Apple: ` + laptopApple);
-console.log(`Laptop Lenovo: ` + laptopLenovo);
+let hp = 15.678;
+let apple = 123.965;
+let asus = 90.2345;
+console.log(`hp price: ` + hp);
+console.log(`apple price: ` + apple);
+console.log(`asus price: ` + asus);
 
-console.log(`Max Price: ` + Math.max(laptopHp, laptopApple, laptopLenovo));
+let ware = [`hp`, `apple`, `asus`];
 
-console.log(`Min price: ` + Math.min(laptopHp, laptopApple, laptopLenovo));
+let maxPrice = Math.max(hp, apple, asus);
+console.log(`Max Price: ` + maxPrice);
 
-let laptopSum = (laptopHp + laptopApple + laptopLenovo);
-console.log(`Sum of laptops: ` + laptopSum);
+let minPrice = Math.min(hp, apple, asus);
+console.log(`Min price: ` + minPrice);
 
-let laptopMathSum = (Math.floor(laptopHp) + Math.floor(laptopApple) + Math.floor(laptopLenovo));
-console.log(`Sum of laptops (floor): ` + laptopMathSum);
+let wareSum = (hp + apple + asus);
+console.log(`Sum of wares: ` + wareSum);
 
-// Выведите сумму товаров округленную до сотен. (Например если получилось 260, то выведите 300)
-let laptopToHundret = laptopSum / 100;
-laptopToHundret = Math.ceil(laptopToHundret);
-console.log(`Sum of laptops (*100): ` + (laptopToHundret * 100));
+let wareMathSum = (Math.floor(hp) + Math.floor(apple) + Math.floor(asus));
+console.log(`Sum of wares (floor): ` + wareMathSum);
 
-console.log(`Multiplicity of laptop sum is: ` + (!!(Math.floor(laptopSum) % 2) == 0));
+let wareSumtoHundret = wareSum / 100;
+wareSumtoHundret = Math.ceil(wareSumtoHundret) * 100;
+console.log(`Sum of wares (*100): ` + (wareSumtoHundret));
 
-let sumCustomerMoney = 500 - laptopSum;
+var multiplicity = !!(Math.floor(wareSum) % 2) == 0;
+console.log(`Multiplicity of ware sum is: ` + multiplicity);
+
+let sumCustomerMoney = 500 - wareSum;
 console.log(`Customer Money: ` + sumCustomerMoney);
 
-let averageValue = laptopSum / 3 ;
+let averageValue = wareSum / ware.length;
 averageValue = averageValue.toFixed(2);
 console.log(`Average value of price: ` + averageValue);
 
 let sale = Math.random();
 console.log(`Sale: ` + (sale.toFixed(2) * 100) + `%`);
+
+let priceSaleHp = hp * sale;
+priceSaleHp = hp - priceSaleHp;
+priceSaleHp = priceSaleHp.toFixed(2);
+console.log(`hp - price with random sale: ` + Math.abs(priceSaleHp));
+
+let priceSaleApple = apple * sale;
+priceSaleApple = apple - priceSaleApple;
+priceSaleApple = priceSaleApple.toFixed(2);
+console.log(`apple - price with random sale: ` + priceSaleApple);
+
+let priceSaleAsus = asus * sale;
+priceSaleAsus = asus - priceSaleAsus;
+priceSaleAsus = priceSaleAsus.toFixed(2);
+console.log(`asus price with random sale: ` + priceSaleAsus);
+
+let costHp = (hp / 2) - priceSaleHp;
+console.log(`Lost profit hp: ` + costHp);
+let costApple = (apple /2) - priceSaleApple;
+console.log(`Lost profit apple: ` + costApple);
+let costAsus = (asus / 2) - priceSaleAsus;
+console.log(`Lost profit asus: ` + costAsus);
+
+console.log(`
+Максимальна ціна: ${maxPrice}
+Мінімальна ціна: ${minPrice}
+Сума цін: ${wareSum}
+Сума заокруглена в меншу сторону: ${wareMathSum}
+Сума заокруглена до сотні: ${wareSumtoHundret}
+Парність числа: ${multiplicity}
+Сума здачі з 500 грн: ${sumCustomerMoney}
+Середнє значення: ${averageValue}
+Знижка: ${sale}
+Ціна зі знижкою hp: ${priceSaleHp}
+Ціна зі знижкою apple: ${priceSaleApple}
+Ціна зі знижкою asus: ${priceSaleAsus}
+Упущена вигода hp: ${costHp}
+Упущена вигода apple: ${costApple}
+Упущена вигода asus: ${costAsus}
+`);
+
+
+
+
+
+
+
+
 
 
 
