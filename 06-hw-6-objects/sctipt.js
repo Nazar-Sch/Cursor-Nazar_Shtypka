9f98815ecf08003a12724145d3176c1f2d79060e
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-02-20 15:49:37 +0200

    hw#6 done

--- 06-hw-6-objects/sctipt.js
+++ 06-hw-6-objects/sctipt.js
@@ -25,8 +25,6 @@ const students = [{
 }];
 
 
-
-
 // Task #1
 const getSubject = studentsSubject => {
   
@@ -43,120 +41,51 @@ const getSubject = studentsSubject => {
   const subjectWithDelChar = letterUp.map((el) => el.split('_').join(' ') );
   return subjectWithDelChar;
 }
-console.log(getSubject(students[0]));
+console.log(getSubject(students[0])); //["Math", "Algorithms", "Data science"]
 
 // Task #2
-const getAverage = (...numbers) => {
-  const getSum = numbers.reduce((previus, current) => Math.round(previus + current));
-  const result = getSum / numbers.length;
-  return result; 
-};
+const getAverageMark = studentsMarks  => {
+    let {subjects} = studentsMarks;
+    let marks = Object.values(subjects);
+    let marksArr = [].concat(...marks);
+    let result = (marksArr.reduce((previus, current) =>  current + previus)) / marksArr.length;
+    return result.toFixed(2);
+  } 
+console.log(`Student's average mark is: ${getAverageMark(students[0])}`);
 
-// const getAverageMark = studentsMark => {
-// let {subjects} = studentsMark;
+// Task #3
+const getStudentsInfo = student => {
+  let {course, name} = student;
+  const infObj = {
+    course,
+    name,
+    averageMark: getAverageMark(student),
+  }
+  return infObj;
+}
+console.log(getStudentsInfo(students[0]));
 
-// console.log(subjects);
-// } 
-// getAverageMark(students[0]);
+// Task #4
+const getStudentsNames = (sortedNames) => {
+  const namesArr = [];
+  students.map((student) => {
+    let {name} = student;
+    namesArr.push(name);
+  });
+  return sortedNames = namesArr.sort();
+}
+console.log(getStudentsNames());
 
-
-
-
-
-
-// const getStudentsName = (student) => {
-//   student = [];
-//   for (let i = 0; i < students.length; i++) {
-//     const {name} = students[i];
-//     student.push(name);
-//   }
-//   console.log(student);
-// }
-// getStudentsName();
-
-// const getStudentSubject = subjectArr => {
-//   subjectsArr = [];
-//   for (let j = 0; j < students.length; j++) {
-//     for(let key in students[j]['subjects']) {
-//       subjectsArr.push(key);
-//     }
-//   }
-//   console.log(subjectsArr);
-// }
-
-// const getStudentSubject = Object.keys(student.subjects)
-// console.log(getStudentSubject);
-
-
-
-
-
-
-
-
-//   const getStudents = students.forEach((el) => {
-//     // for(let key in el['name']) {
-//     //   name.push(key);
-//     // }
-//     // console.log(name);
-//   });
-
-// getStudents();
-
-
-  // name = students[0];
-  // const subjectsArr = [];
-  
-  // for(let key in name['subjects']) {
-  //   subjectsArr.push(key);
-  // }
-  // console.log(name['name'] + ' ' + subjectsArr);
-
-
-
-// }
-// getSubject();
-
-
-
-
-
-// console.log(Object.entries(students));
-// Object.entries(students).map(([name, subjects]) => console.log(name, subjects));
-// console.log(Object.values(students));
-// console.log(Object.values(...students));
-// TASK #1
-// const getSubject = (name) => {
-// }
-// getSubject(students);
-
-// const user = {
-//   name: 'Bogdan',
-//   status: 'lector',
-//   createdBy: 'iVan',
-// }
-// const {name, status, ...other} = user;
-// // console.log(other);
-
-// function qwe() {
-//   const userName = {name, status};
-//   console.log(userName);
-// }
-// qwe();
-
-
-
-
-// const students = [{
-//   name: "Tanya",
-//   course: 3,
-//   subjects: {
-//     math: [4, 4, 3, 4],
-//     algorithms: [3, 3, 3, 4, 4, 4],
-//     data_science: [5, 5, 3, 4]
-//   },
-// }];
-
-// const copy = Object.assign({}, students);
-// console.log(copy);
-
+// Task #5
+const calculateWordLetters = (str) => {
+  const letter = {};
+  for(let i = 0; i <= str.length; i++) {
+    const char = str[i];
+    if(!letter[char]) {
+      letter[char] = 0;
+    }
+    letter[char]++;
+  }
+  return letter;
+}
+console.log(calculateWordLetters('Programming'));
