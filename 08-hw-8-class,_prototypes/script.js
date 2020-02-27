9263d3470d9a90c7506448742ea1d023131de204
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-02-27 14:21:15 +0200

    started hw#8

--- 08-hw-8-class,_prototypes/script.js
+++ 08-hw-8-class,_prototypes/script.js
@@ -1,27 +1,62 @@
+// HW#8 (!) Homework should be submitted as a page on GITPAGES.
+// 1. Students must have the following properties: university, course, fullName, they are passed when creating a student 
+// (in the constructor).
+// 2. Create this.getInfo () method -> "Odesa Higher Fraud High School 1st year student, Ostap Rodomanskiy Bender", 
+// the method returns the aggregate information about the course, the institution and the student's name.
+// 3. Create a getter of this.marks, which returns an array of student grades [5, 4, 4, 5].
+// 4. Create an assessment setter this.marks = 5 that allows you to grade the student. 
+// After the evaluation is made, the getter must return this.marks -> array [5, 4, 4, 5, 5].
+// 5. Create a method to get this.getAverageMark () -> 4.6 grade point average.Create
+// 6. Create this.dismiss method that will "exclude" the student. After calling this method - it is impossible to put 
+// the student grades and to receive them any more. (No errors, just always returns null instead of array of estimates).
+// 7. Create this.recover method that will allow you to recover the student.
+
+
 class Student {
-    constructor(univercity, course, fullName) {
+    constructor(univercity, course, fullName, marks) {
         this.univercity = univercity;
         this.course = course;
         this.fullName = fullName;
-        this.getInfo = () => `${this.course} ${this.univercity} ${this.fullName}`;
+        this.marks = marks;
+        // this.getAverageMark = getAverageMark;
     }
-    // getInfo() {
-    //     return `${this.course} ${this.univercity} ${this.fullName}`
-    // }
-
-    get marks() {
-        const studentsMarks = []
-        return studentsMarks;
+    getInfo() {
+        return `${this.course} ${this.univercity} ${this.fullName}`;
     }
 
-    set marks(value) {
-        studentsMarks.push(value);
+    get studentsMarks() {
+        return this.marks;
     }
 
-    getAverageMark(min, max) {
-        return Math.floor(Math.random() * (max - min) + min);
+    set studentsMarks(value) {
+        return this.marks.push(value);
+    }
+    getAverageMark() {
+        // return Math.floor(Math.random() * (max - min) + min);
+        return this.marks.reduce((acc, val) => (acc + val), 0)/this.marks.length;
     }
 
     
 }
+const student = new Student(`Національний Університет 'Львівська політехніка'`, `1 курс`, `Назар Назарович`, [5,4,4,5]);
+
+// Get info
+console.log(`Students info: ${student.getInfo()}`);
+document.writeln(`<hr> Students info: ${student.getInfo()} <hr>`);
+
+// Get students marks
+console.log(`Students marks: ${student.marks}`);
+document.writeln(`<hr> Students marks: ${student.marks} <hr>`);
+
+// Add new mark
+student.studentsMarks = 5;
+console.log(`Students marks: ${student.marks}`);
+document.writeln(`<hr> Students marks: ${student.marks} <hr>`);
+
+// Get average mark
+console.log(`Students average mark: ${student.getAverageMark()}`);
+document.writeln(`<hr> Students average mark: ${student.getAverageMark()} <hr>`);
+
+// Get dismiss
+
 
