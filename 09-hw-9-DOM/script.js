5b86e8bfd43e6a9bdc5893f9b0ad053c7834ba2d
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-03-03 23:46:13 +0200

    hw#9 done

--- 09-hw-9-DOM/script.js
+++ 09-hw-9-DOM/script.js
@@ -3,3 +3,33 @@
 // Advanced so that the squares change color once per second.
 // In order for the squares to appear on the page and begin to change, you need to call the generateBlocksInterval () function.
 
+function generateBlocks() {
+    const getRandomColor = () => {
+        return `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+    }
+    function createBlocks() {
+        const parentSquare = document.getElementById('parentSquare');
+        parentSquare.innerHTML = '';
+        for(let i = 0; i < 25; i++) {
+            const block = document.createElement('div');
+            block.style.height = "50px";
+            block.style.width = "50px";
+            block.style.backgroundColor = getRandomColor();
+            parentSquare.append(block);
+        }
+        return parentSquare;
+    }
+    return createBlocks();
+}
+
+function generateBlocksInterval() {
+    return setInterval(() => {
+        generateBlocks();
+    }, 1000); 
+}
+
+generateBlocks();
+generateBlocksInterval();
+
+
+
