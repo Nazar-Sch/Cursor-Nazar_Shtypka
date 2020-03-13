c9ef841ec11ec9d03ac9f3b60a8afe778162f87b
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-03-13 21:13:48 +0200

    hw11 corrected

--- 11-hw-11-Promise/script.js
+++ 11-hw-11-Promise/script.js
@@ -1,17 +1,17 @@
 const TIME_OUT = 50;
 async function getRandomChinese(length) {
-        const time = length * TIME_OUT;
-        let chineseWord = '';
-        for (let i = 0; i < length; i++) {
-        let newPromise = new Promise((resolve) => {
+    const time = length * TIME_OUT;
+    let chineseWord = '';
+    for (let i = 0; i < length; i++) {
+        const newPromise = new Promise((resolve) => {
             setTimeout(() => {
                 let sign = Date.now().toString().substr(-5);
                 resolve(String.fromCharCode(sign))
             }, time);
         })
-        await newPromise.then(letter => {
-            chineseWord += letter;
-        })
+    await newPromise.then(letter => {
+        chineseWord += letter;
+    })
     }
     console.log(`
     Random letters: ${chineseWord}. 
