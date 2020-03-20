a130dd1527a38c440b883f6f0d4650957c318459
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-03-20 14:04:12 +0200

    added planets

--- 12-hw-12-Server/script.js
+++ 12-hw-12-Server/script.js
@@ -1,41 +1,125 @@
 const BASE_URL = 'https://swapi.co/api';
+// const images = {
+//     'https://swapi.co/api/people/1/': './img/luke.webp',
+//     'https://swapi.co/api/people/2/': './img/C-3PO.webp',
+//     'https://swapi.co/api/people/3/': './img/R2-D2.webp',
+//     'https://swapi.co/api/people/4/': './img/Darth-Vader.webp',
+//     'https://swapi.co/api/people/5/': './img/Leia-Organa.webp',
+//     'https://swapi.co/api/people/10/': './img/Obi-Wan-Kenobi.webp',
+//     'https://swapi.co/api/people/13/': './img/Chewbacca.webp',
+//     'https://swapi.co/api/people/14/': './img/Han-Solo.webp',
+//     'https://swapi.co/api/people/16/': './img/Jabba.webp',
+//     'https://swapi.co/api/people/18/': './img/Wedge-Antilles.webp',
+//     'https://swapi.co/api/people/20/': './img/Yoda.webp',
+//     'https://swapi.co/api/people/21/': './img/Palpatine.webp',
+//     'https://swapi.co/api/people/22/': './img/Boba-Fett.webp',
+//     'https://swapi.co/api/people/23/': './img/IG-88.webp',
+//     'https://swapi.co/api/people/24/': './img/Bossk.webp',
+// }
+const loader = document.getElementById('loader');
+const filmI = document.getElementById('filmI').addEventListener('click', () => getCharacterFromAPI(1).then(renderCharacters).finally(() => {
+    document.getElementById('planets').classList.add('hidden');
+    getHiddenLoader();
+    getActiveCharacters();
+}));
+const filmII = document.getElementById('filmII').addEventListener('click', () => getCharacterFromAPI(2).then(renderCharacters).finally(() => {
+    document.getElementById('planets').classList.add('hidden');
+    getHiddenLoader();
+    getActiveCharacters();
+}));
+const filmIII = document.getElementById('filmIII').addEventListener('click', () => getCharacterFromAPI(3).then(renderCharacters).finally(() => {
+    document.getElementById('planets').classList.add('hidden');
+    getHiddenLoader();
+    getActiveCharacters();
+}));
+const filmIV = document.getElementById('filmIV').addEventListener('click', () => getCharacterFromAPI(4).then(renderCharacters).finally(() => {
+    document.getElementById('planets').classList.add('hidden');
+    getHiddenLoader();
+    getActiveCharacters();
+}));
+const filmV = document.getElementById('filmV').addEventListener('click', () => getCharacterFromAPI(5).then(renderCharacters).finally(() => {
+    document.getElementById('planets').classList.add('hidden');
+    getHiddenLoader();
+    getActiveCharacters();
+}));
 
-const filmI = document.getElementById('filmI');
-const filmII = document.getElementById('filmII');
-const filmIII = document.getElementById('filmIII');
-const filmIV = document.getElementById('filmIV');
-const filmV = document.getElementById('filmV');
-
-filmI.addEventListener('click', function() {
-    return getCharacter(1).then(renderCharacters); 
-});
-
-async function getCharacter(film) {
+async function getCharacterFromAPI(film) {
+    loader.classList.add('active');
     const res = await axios.get(BASE_URL + `/films/${film}/`);
-    console.log(res.data.characters);
-
-    return res.data.characters.map((el) => {
-        const promiseAx = axios.get(el).then((char) => char.data);
-        console.log(promiseAx);
-        return promiseAx;
-    });
+    return Promise.all(res.data.characters.map(async (el) => {
+        const response = await axios.get(el).then((char) => char.data);
+        return response;
+    }));
 }
-function renderCharacters(characters) {
-    // innerHTML = '';
-    const container = document.querySelector('.characters');
-    console.log(container);
-    characters.forEach(character => {
-        const divEl = document.createElement('div');
-        divEl.classList = 'charList';
-        divEl.innerHTML = `
-        <h4>Name: ${character.name}</h4>
-        <h5>Birth Year: ${character.birth_year}</h5>
-        <h5>Gender: ${character.gender}</h5>
+
+function renderCharacters(response) {
+    let container = document.querySelector('.characters');
+    container.innerHTML = '';
+    response.forEach(response => {
+        const charactersInfo = document.createElement('div');
+        charactersInfo.classList = 'charList';
+        charactersInfo.innerHTML = `
+        <p><span class="colorBlue">Name:</span> ${response.name}</p>
+        <p><span class="colorBlue">Birth Year:</span> ${response.birth_year}</p>
+        <p><span class="colorBlue">Gender:</span> ${response.gender}</p>
         `;
-        container.append(divEl);
+        container.append(charactersInfo);
     })
 }
+function getActiveCharacters() {
+    document.getElementById('characters').classList.remove('hidden');
+}
 
-// getCharacter().then(renderCharacters);
+function getHiddenLoader(button) {
+    loader.classList.remove('active');
+}
 
 
+// PLANET
+let currentPage = 1;
+async function getPlanets(currentPage) {
+    const res = await axios.get(BASE_URL + `/planets/?page=${currentPage}`);
+    console.log(res.data.results);
+    return res.data.results;
+}
+
+function renderPlanets(planets) {
+    let container = document.querySelector('.planets');
+    container.innerHTML = '';
+    planets.forEach(response => {
+        let planetDivEl = document.createElement('div');
+        planetDivEl.classList = 'planetsInfo';
+        planetDivEl.innerHTML = `
+        <p><span class="colorBlue">Name:</span> ${response.name}</p>
+        `;
+        container.append(planetDivEl);
+    })
+    
+}
+
+const planet = document.getElementById('planet').addEventListener('click', function() {
+    loader.classList.add('active');
+    document.querySelector('.characters').classList.add('hidden');
+    document.getElementById('planets').classList.remove('hidden');
+    return getPlanets(currentPage).then(renderPlanets).finally(getHiddenLoader);
+});
+
+
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+prev.addEventListener('click', function() {
+    loader.classList.add('active');
+    if (currentPage != 1) {
+        currentPage--;
+        getPlanets(currentPage).then(renderPlanets).finally(getHiddenLoader);
+    }
+});
+
+next.addEventListener('click', function() {
+    loader.classList.add('active');
+if (currentPage != 6) {
+    currentPage++;
+    getPlanets(currentPage).then(renderPlanets).finally(getHiddenLoader);
+}
+});
