533e3744efaae458bb8bfe8cf3c479d0c1993974
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-03-22 02:56:54 +0200

    hw#13 done

--- 13-hw-13-generators/script.js
+++ 13-hw-13-generators/script.js
@@ -1,14 +1,27 @@
-function* createIdGenerator() {
-    let id = 0;
-    while (true) {
-      yield id++;
+function* createIdGenerator(id) {
+  while(true) {
+    document.querySelector('.outputId').innerHTML = `${id}`;
+    yield id++;
+  }
+}
+const idGenerator = createIdGenerator(1);
+document.querySelector('.idGenerator').addEventListener('click', () => idGenerator.next().value);
+
+function* newFontGenerator(size) {
+  while (true) {
+    let result = yield size;
+    if(result === "up") {
+      size += 2;
+      document.querySelector('.text').style.fontSize = size + "px";
+    }
+    if(result === "down") {
+      size -= 2;
+      document.querySelector('.text').style.fontSize = size + "px";
     }
   }
-  const idGenerator = createIdGenerator();
-  idGenerator.next().value;
 
-let plus = document.getElementById('plus').addEventListener('click', getPlus);
-let minus = document.getElementById('minus').addEventListener('click', getPlus);
+}
 
-
-
+const fontGenerator = newFontGenerator(14); 
+let plus = document.getElementById('up').addEventListener('click', () => fontGenerator.next("up").value);
+let minus = document.getElementById('down').addEventListener('click', () => fontGenerator.next("down").value);
