729b2c38d6d0539b906d0b3646b2b94aa7f2eba0
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-03-28 00:30:10 +0200

    Updates

---
+++ precache-manifest.660f287eb5e57770d24a9da463490faa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0512c5a76a62f00514f37af2d2049a2",
+    "url": "/Nazar-Sch/Cursor-Nazar_Shtypka/index.html"
+  },
+  {
+    "revision": "40811f5bc601c814495a",
+    "url": "/Nazar-Sch/Cursor-Nazar_Shtypka/static/css/main.1cddaed9.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/Nazar-Sch/Cursor-Nazar_Shtypka/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nazar-Sch/Cursor-Nazar_Shtypka/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40811f5bc601c814495a",
+    "url": "/Nazar-Sch/Cursor-Nazar_Shtypka/static/js/main.c6d2bb31.chunk.js"
+  },
+  {
+    "revision": "cb36540a04da2806b5a6",
+    "url": "/Nazar-Sch/Cursor-Nazar_Shtypka/static/js/runtime-main.a25ddb82.js"
+  },
+  {
+    "revision": "115e282b285a98c23bf0ae44c51d3d76",
+    "url": "/Nazar-Sch/Cursor-Nazar_Shtypka/static/media/down-arrow.115e282b.svg"
+  }
+]);
