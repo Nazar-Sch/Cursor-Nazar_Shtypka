97df2987343d871191fd623b6f6bd24e79001435
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-03-28 00:14:20 +0200

    hw15 done

--- 15-hw-15-react-basic/my-app/src/App.js
+++ 15-hw-15-react-basic/my-app/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Post from "./components/post";
+import './components/post/style.css'
 function App() {
-  const name = 'Vasa Pupkin';
   return (
-    <div className="App">
-      <div>
-  <p>Hello {name}</p>
-        <button>Active</button>
-      </div>
-    </div>
-  );
+  <div className="App">
+    <Post
+    name='Anakin Skywalker'
+    photo='https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg'
+    nickname="@dart_vader"
+    content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+    image="https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale"
+    date="26 февр."
+    comments="486"
+    share="146"
+    like="887"
+    />
+  </div>
+  )
 }
 
 export default App;
---
+++ 15-hw-15-react-basic/my-app/src/components/post/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+function post(props){
+        return (
+            <div className='post'>
+                <div className="user">
+                <div className='userInfo'>
+                    <div className='photoUser'>
+                        <img src={props.photo} className="photoUser__item"/>
+                    </div>
+                    <div className='userDate'>
+                        <div className="userName">
+                        <span>
+                            {props.name}
+                        </span>
+                        <i className="fas fa-check-circle"></i>
+                        <div className="userNick"> 
+                        <span>{props.nickname}</span>·<span className='datePost'>{props.date}</span>
+                        </div>
+                        </div>
+                    <div className='content'>
+                        {props.content}
+                    </div>
+                    </div>
+                </div>
+                    <button className='hidePost'></button>
+                    </div>
+                
+                <div className='imagePost'>
+                    <img src={props.image} className="imagePost__item"/>
+                
+                <div className="imageFooter">
+                    <div className="btn-postFooter comments">
+                        <button className="btn-comments btn-post__likeItems">
+                        <i className="far fa-comment fa-2x"></i>
+                        </button>
+                        {props.comments}
+                    </div>
+                    <div className="btn-postFooter comments">
+                        <button className="btn-share btn-post__likeItems">
+                        <i className="fas fa-retweet fa-2x"></i>
+                        </button>
+                        {props.share}
+                    </div>
+                    <div className="btn-postFooter comments">
+                        <button className="btn-like btn-post__likeItems">
+                        <i className="far fa-heart fa-2x"></i>
+                        </button>
+                        {props.like}
+                    </div>
+                    <div className="btn-postFooter comments">
+                        <button className="btn-download btn-post__likeItems">
+                        <i className="fas fa-download fa-2x"></i>
+                        </button>
+                    </div>
+
+                </div>
+                </div>
+            </div>
+        )
+    
+}
+
+export default post;
